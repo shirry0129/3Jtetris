61445817a1ed87655748db448c4de5f0b94d4287
shirry0129 <shimizy35@gmail.com>
2017-06-23 17:17:32 +0900

    テトリスの初期状態を設定

--- tetris/cell.h
+++ tetris/cell.h
@@ -4,6 +4,6 @@
 
 typedef struct {
 	bool is_buried = false;
-	Color cell_color = Color(Palette::Yellowgreen);
+	Color cell_color = Color(255, 255, 255, 0);
 	Rect cell;
 }Cell;
--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -11,42 +11,49 @@ Tetris::Tetris(wchar shape){
 		field[1][1].is_buried = true;
 		field[2][1].is_buried = true;
 		field[3][1].is_buried = true;
+		tetris_color = Color(Palette::Lightblue);
 		break;
 	case 'O':
 		field[1][1].is_buried = true;
 		field[2][1].is_buried = true;
 		field[1][2].is_buried = true;
 		field[2][2].is_buried = true;
+		tetris_color = Color(Palette::Yellow);
 		break;
 	case 'S':
 		field[0][2].is_buried = true;
 		field[1][2].is_buried = true;
 		field[1][1].is_buried = true;
 		field[2][1].is_buried = true;
+		tetris_color = Color(Palette::Green);
 		break;
 	case 'Z':
 		field[0][1].is_buried = true;
 		field[1][1].is_buried = true;
 		field[1][2].is_buried = true;
 		field[2][2].is_buried = true;
+		tetris_color = Color(Palette::Red);
 		break;
 	case 'J':
 		field[0][1].is_buried = true;
 		field[0][2].is_buried = true;
 		field[1][2].is_buried = true;
 		field[2][2].is_buried = true;
+		tetris_color = Color(Palette::Blue);
 		break;
 	case 'L':
 		field[0][2].is_buried = true;
 		field[1][2].is_buried = true;
 		field[2][2].is_buried = true;
 		field[2][1].is_buried = true;
+		tetris_color = Color(Palette::Orange);
 		break;
 	case 'T':
 		field[1][1].is_buried = true;
 		field[0][2].is_buried = true;
 		field[1][2].is_buried = true;
 		field[2][2].is_buried = true;
+		tetris_color = Color(Palette::Purple);
 		break;
 	default:
 		break;
